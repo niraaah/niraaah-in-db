121b6d541c8a56456352557432c7b0c02a72ecf7
niraaah <meer@kakao.com>
2024-12-18 18:41:51 +0900

    fix : 데이터 로드할때 상위 파일들 참조하도록

--- services/dataLoader.py
+++ services/dataLoader.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pandas as pd
 from database.connectionManager import DatabaseManager
 from services.jobProcessor import JobDataProcessor
